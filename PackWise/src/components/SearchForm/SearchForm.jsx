a69ccf53b35b125ad38f84148d68461e39d860e9
Jhondrek <jhondrek116@gmail.com>
2025-10-22 15:18:21 -0600

    Add form validation, error messages, and conditional submit button visibility
    - Implemented full form validation for all input sections
    - Added and styled alert text for invalid input values
    - Made the submit button visible only when the user reaches the final form section

--- PackWise/src/components/SearchForm/SearchForm.jsx
+++ PackWise/src/components/SearchForm/SearchForm.jsx
@@ -6,25 +6,73 @@ import { faCircleUp, faCircleDown } from "@fortawesome/free-solid-svg-icons";
 
 export default function SearchForm(){
 
-    const [isSecondElementEntered, setIsSecondElementEntered] = useState([false, false])
+    const [formProgressState, setFormProgressState] = useState([false, false])
 
     //React-Hook-Form validation
 
-    const{register, handleSubmit, watch, formState: {errors}} = useForm({defaultValues:{
+    const{register, handleSubmit, watch, trigger, clearErrors, formState: {errors}} = useForm({defaultValues:{
         originCountry:"",
-        arravingCountry:""
-    }})
-    
+        arrivingCountry:"",
+        placeToExplore:"",
+        startDate: "",
+        endDate:"",
+        numberOfTravelers:"",
+        USDBudgetPerPerson:""
+    },
+
+})
+
+    console.log(watch())
+      
     function submitForm(data){
-        console.log("handleSubmit triggered", data)
+        console.log("handleSubmit triggered", errors)
     }
 
-    console.log("Watch values : ", watch())
+    console.log("Watch values : ", errors)
+
+    async function areCurrentInputsValid() {
+        
+        const currentErrorObject = await getCurrentErrorsState()
+        console.log(currentErrorObject)
+
+        if(formProgressState[1] === false && currentErrorObject){
+            return "tercera true" 
+        }
+        if(formProgressState[1] === false && currentErrorObject){
+            return true
+        }else if(formProgressState[0] === false && currentErrorObject){
+            return true
+        }
+        return false
+    }
+
+    async function getCurrentErrorsState(){
+        const firstInputSection = await trigger(["originCountry", "arrivingCountry", "placeToExplore"])
+        const secondInputSection = await trigger(["startDate", "endDate"])
+        const thirdInputSection = await trigger(["numberOfTravelers", "USDBudgetPerPerson"])
+        console.log(firstInputSection)
+        // Si estás en la primera parte del formulario
+  if (formProgressState[0] === false) {
+    return firstInputSection;
+  }
+
+  // Si estás en la segunda parte del formulario
+  if (formProgressState[0] === true && formProgressState[1] === false) {
+    return secondInputSection;
+  }
+
+  // Si estás en la tercera parte
+  if (formProgressState[1] === true) {
+    return thirdInputSection;
+  }
+
+  return false;
+    }
 
     //back button
 
     function showlLastInputs(){
-        setIsSecondElementEntered((prev)=>{
+        setFormProgressState((prev)=>{
 
             const newPrev = [...prev]
         
@@ -40,8 +88,9 @@ export default function SearchForm(){
          )
     }
 
-    function showNextInputPairs(){
-        setIsSecondElementEntered((prev)=>{
+   async function showNextInputPairs(){
+        if(await areCurrentInputsValid()){
+            setFormProgressState((prev)=>{
 
             const newPrev = [...prev]
         
@@ -51,8 +100,11 @@ export default function SearchForm(){
                     break
                 }
             }
+            clearErrors()
             return newPrev
         })
+        }
+        
     }
 
     
@@ -65,7 +117,7 @@ export default function SearchForm(){
     function handleEnterKey(e){
         if(e.key === "Enter"){
             showNextInputPairs()
-                    console.log(isSecondElementEntered)
+                    console.log(formProgressState)
 
         }
     }
@@ -74,45 +126,70 @@ export default function SearchForm(){
 
     
 
-console.log(isSecondElementEntered)
+console.log(formProgressState)
 
     return(
 
         <form onSubmit={handleSubmit(submitForm)} className={style.form}>
-            <FontAwesomeIcon icon={faCircleUp} className={`${style.form__CircleUpIcon} ${isSecondElementEntered[0]  ? style.visible  : undefined}`} onClick={()=>{isSecondElementEntered[0] && showlLastInputs()}} />
+            <FontAwesomeIcon icon={faCircleUp} className={`${style.form__CircleUpIcon} ${formProgressState[0]  ? style.visible  : undefined}`} onClick={()=>{formProgressState[0] && showlLastInputs()}} />
            
-            <section className={`${style.form__locationsCont} ${isSecondElementEntered[0] && style.completed}`}>
-                <input {...register("originCountry", {required:"Please enter a country"})} type="text"   placeholder="Origin country"  />
-                <input {...register("arraivingCountry, ", {required:"Please enter a country"})} type="text"   placeholder="Arriving country"  />
-                <input {...register("placeToExplore", {required:"Please enter a place to explore"})} onBlur={ !isSecondElementEntered[0] ? handleOnBlur : undefined} onKeyDown={(e)=> !isSecondElementEntered[0] && handleEnterKey(e)} className={style.form__travelAreaInput}  type="text" placeholder="Place to explore" />
+            <section  className={`${style.form__locationsCont} ${formProgressState[0] && style.completed}` }>
+                <div>
+                    <input {...register("originCountry", {required:"Please enter a country"})} type="text"   placeholder="Origin country"  />
+                    <p className={style.form__error}> {errors.originCountry && errors.originCountry.message}</p>
+                </div>
+                <div>
+                    <input {...register("arrivingCountry", {required:"Please enter a country"})} type="text"   placeholder="Arriving country"  />
+                    <p className={style.form__error}> { errors.arrivingCountry && errors.arrivingCountry.message}</p>
+                </div>
+                <div>
+                    <input {...register("placeToExplore", {required:"Please enter a place to explore"})}  className={style.form__travelAreaInput}  type="text" placeholder="Place to explore" onBlur={ !formProgressState[0] ? handleOnBlur : undefined} onKeyDown={(e)=> !formProgressState[0] && handleEnterKey(e)}/>
+                    <p className={style.form__error}> {errors.placeToExplore && errors.placeToExplore.message}</p>
+                    
+                </div>
+
+                
             </section>
 
-           <section className={`${style.form__datesCont} ${ isSecondElementEntered[0] && style.visible} ${ isSecondElementEntered[1] && style.completed}`}>
-                <div className={style.form__dateCont}>
-                    <input {...register("startDate", {required:"Please enter the start date"})} type="date" name="startDate" />
-                    <label htmlFor="startDate" >Start date</label>
+           <section className={`${style.form__datesCont} ${ formProgressState[0] && style.visible} ${ formProgressState[1] && style.completed}`}>
+                <div >
+                    <div className={style.form__dateCont}>
+                        <input {...register("startDate", {required:"Please enter the start date"})} type="date" name="startDate" />
+                        <label htmlFor="startDate" >Start date</label>
+                    </div>
+                    <p className={style.form__error}> {errors.startDate && errors.startDate.message}</p>
                 </div>
-                <div className={style.form__dateCont}>
-                    <input {...register("endDate", {required:"Please enter the end date"})} type="date" name="endDate"  onBlur={!isSecondElementEntered[1] ? handleOnBlur : undefined} onKeyDown={(e)=> !isSecondElementEntered[0] && handleEnterKey(e)}/>
-                    <label  htmlFor="endDate" >End date</label>
-                </div>
-            </section>
-
-            <section className={`${style.form__TravelerDataCont} ${isSecondElementEntered[1] && style.visible}`}>
-                <div className={style.form__TravelerDataCont__travelerData}>
-                    <label {...register("numberOfTravelers", {required:"Please enter the number of travelers"})} htmlFor="">Number of travelers</label>
-                    <input type="number"/>
-                </div>
-
-                <div className={style.form__TravelerDataCont__travelerData}>
-                    <input type="number"/>
-                    <label {...register("USDBudgetPerPerson", {required:"Please enter USD budget per person", minLength:{value:3, message: "Min length is 3"}})} htmlFor="">USD budget per person</label>
+                <div >
+                    <div className={style.form__dateCont}>
+                        <input {...register("endDate", {required:"Please enter the end date"})} type="date" name="endDate"  onBlur={!formProgressState[1] ? handleOnBlur : undefined} onKeyDown={(e)=> !formProgressState[0] && handleEnterKey(e)}/>
+                        <label  htmlFor="endDate" >End date</label>
+                    </div>
+                    <p className={style.form__error}> {errors.endDate && errors.endDate.message}</p>
                 </div>
             </section>
 
-            <FontAwesomeIcon icon={faCircleDown} className={`${style.form__CircleDownIcon} ${isSecondElementEntered[1] && style.hidden}`} onClick={()=>showNextInputPairs()}/>
+            <section className={`${style.form__TravelerDataCont} ${formProgressState[1] && style.visible}`}>
+                <div >
+                    <div className={style.form__TravelerDataCont__travelerData}>
+                        <label  htmlFor="">Number of travelers</label>
+                        <input {...register("numberOfTravelers", {required:"Please enter the number of travelers"})} type="number"/>
+                    </div>
+                    <p className={style.form__error}> {errors.numberOfTravelers && errors.numberOfTravelers.message}</p>
+                </div>
 
-            <button className={style.formContainer__button}>Plan My Trip</button>
+                <div >
+                    <div className={style.form__TravelerDataCont__travelerData}>
+                        <input {...register("USDBudgetPerPerson", {required:"Please enter USD budget per person", min: {value:100, message: "minimum value is 100"}})} type="number"/>
+                        <label  htmlFor="">USD budget per person</label>
+                    </div>
+                    <p className={style.form__error}> {errors.USDBudgetPerPerson && errors.USDBudgetPerPerson.message}</p>
+                </div>
+            </section>
+
+            <FontAwesomeIcon icon={faCircleDown} className={`${style.form__CircleDownIcon} ${formProgressState[1] && style.hidden}`} onClick={()=>showNextInputPairs()}/>
+
+            <button className={`${style.formContainer__button} ${formProgressState[1]=== true && style.visible }`}>Plan My Trip</button> 
+
         </form>
     )
 }                                             
