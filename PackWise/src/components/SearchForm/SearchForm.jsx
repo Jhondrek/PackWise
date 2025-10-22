e4e8d06492fc08a72ab767e0c06f833e385a2a65
Jhondrek <jhondrek116@gmail.com>
2025-10-21 19:17:36 -0600

    npm run build
    Build the project to try to deploy it on netlify through github

--- PackWise/src/components/SearchForm/SearchForm.jsx
+++ PackWise/src/components/SearchForm/SearchForm.jsx
@@ -1,18 +1,25 @@
 import style from "./SearchForm.module.css"
+import { useForm } from "react-hook-form"
 import { useState } from "react";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome"
 import { faCircleUp, faCircleDown } from "@fortawesome/free-solid-svg-icons";
 
 export default function SearchForm(){
 
-    const [formInformation, setFormInformation] = useState("")
     const [isSecondElementEntered, setIsSecondElementEntered] = useState([false, false])
+
+    //React-Hook-Form validation
+
+    const{register, handleSubmit, watch, formState: {errors}} = useForm({defaultValues:{
+        originCountry:"",
+        arravingCountry:""
+    }})
     
-    function handleSubmit(event){
-        event.preventDefault()
-        console.log("handleSubmit triggered", formInformation)
+    function submitForm(data){
+        console.log("handleSubmit triggered", data)
     }
 
+    console.log("Watch values : ", watch())
 
     //back button
 
@@ -71,37 +78,35 @@ console.log(isSecondElementEntered)
 
     return(
 
-        <form onSubmit={handleSubmit} className={style.form}>
+        <form onSubmit={handleSubmit(submitForm)} className={style.form}>
             <FontAwesomeIcon icon={faCircleUp} className={`${style.form__CircleUpIcon} ${isSecondElementEntered[0]  ? style.visible  : undefined}`} onClick={()=>{isSecondElementEntered[0] && showlLastInputs()}} />
            
             <section className={`${style.form__locationsCont} ${isSecondElementEntered[0] && style.completed}`}>
-                                
-
-                <input type="text" id="country"  placeholder="Origin country" onChange={(e)=>setFormInformation(e.target.value)} />
-                <input type="text" id="country"  placeholder="Arriving country" onChange={(e)=>setFormInformation(e.target.value)} />
-                <input onBlur={ !isSecondElementEntered[0] ? handleOnBlur : undefined} onKeyDown={(e)=> !isSecondElementEntered[0] && handleEnterKey(e)} className={style.form__travelAreaInput}  type="text" placeholder="Place to explore" onChange={(e)=>setFormInformation(e.target.value)}/>
+                <input {...register("originCountry", {required:"Please enter a country"})} type="text"   placeholder="Origin country"  />
+                <input {...register("arraivingCountry, ", {required:"Please enter a country"})} type="text"   placeholder="Arriving country"  />
+                <input {...register("placeToExplore", {required:"Please enter a place to explore"})} onBlur={ !isSecondElementEntered[0] ? handleOnBlur : undefined} onKeyDown={(e)=> !isSecondElementEntered[0] && handleEnterKey(e)} className={style.form__travelAreaInput}  type="text" placeholder="Place to explore" />
             </section>
 
            <section className={`${style.form__datesCont} ${ isSecondElementEntered[0] && style.visible} ${ isSecondElementEntered[1] && style.completed}`}>
                 <div className={style.form__dateCont}>
-                    <input type="date" name="startDate" />
+                    <input {...register("startDate", {required:"Please enter the start date"})} type="date" name="startDate" />
                     <label htmlFor="startDate" >Start date</label>
                 </div>
                 <div className={style.form__dateCont}>
-                    <input type="date" name="endDate"  onBlur={!isSecondElementEntered[1] ? handleOnBlur : undefined} onKeyDown={(e)=> !isSecondElementEntered[0] && handleEnterKey(e)}/>
+                    <input {...register("endDate", {required:"Please enter the end date"})} type="date" name="endDate"  onBlur={!isSecondElementEntered[1] ? handleOnBlur : undefined} onKeyDown={(e)=> !isSecondElementEntered[0] && handleEnterKey(e)}/>
                     <label  htmlFor="endDate" >End date</label>
                 </div>
             </section>
 
             <section className={`${style.form__TravelerDataCont} ${isSecondElementEntered[1] && style.visible}`}>
                 <div className={style.form__TravelerDataCont__travelerData}>
-                    <label htmlFor="">Number of travelers</label>
+                    <label {...register("numberOfTravelers", {required:"Please enter the number of travelers"})} htmlFor="">Number of travelers</label>
                     <input type="number"/>
                 </div>
 
                 <div className={style.form__TravelerDataCont__travelerData}>
                     <input type="number"/>
-                    <label htmlFor="">USD budget per person</label>
+                    <label {...register("USDBudgetPerPerson", {required:"Please enter USD budget per person", minLength:{value:3, message: "Min length is 3"}})} htmlFor="">USD budget per person</label>
                 </div>
             </section>
 
